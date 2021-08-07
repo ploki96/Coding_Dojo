11e06a54f613b9958eab8782a268de8bfd5d58e7
Timothy Wang <timmywang47@yahoo.com>
2021-08-07 19:44:26 -0400

    files for counter assignment

---
+++ Python/flask/fundamentals/Counter/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+@app.route('/')
+def index():
+    if 'counter' not in session:
+        session['counter'] = 0
+    if 'visits' not in session:
+        session['visits'] = 0
+    session['visits'] += 1
+    return render_template("index.html")
+
+@app.route('/visits')
+def visits():
+
+    print("Page visited" + str(session['visits']) + "times")
+    return render_template('visits.html')
+
+@app.route('/increase')
+def increment_counter():
+    print(request.form)
+    session['counter'] += 1
+    return redirect('/')
+
+@app.route('/reset')
+def reset_counters():
+    session.clear('counter')
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)   
+
+
+
+
+
+
