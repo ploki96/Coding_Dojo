6eecf18b2438e4412e56fcc71c90e673f25bca6d
Timothy Wang <timmywang47@yahoo.com>
2021-08-14 15:15:11 -0400

    dojo survey assignment in fundamentals

--- Python/flask/fundamentals/Dojo_Survey/server.py
+++ Python/flask/fundamentals/Dojo_Survey/server.py
@@ -5,20 +5,33 @@ app.secret_key = 'secret_key'
 
 @app.route('/')
 def index():
-    
+    if 'user_name' not in session:
+        session['user_name'] = ""
+    if 'location' not in session:
+        session['location'] = ""
+    if 'language' not in session:
+        session['language'] = ""
+    if 'nuggies' not in session:
+        session['nuggies'] = ""
+    if 'comment' not in session:
+        session['comment'] = ""
     return render_template("index.html")
 
-@app.route('/results', methods=['GET'])
-def guess():
-    if 'user_name' not in session:
-        session['user_name'] = request.form['user_name']
-    if 'location' not in session:
-        session['location'] = request.form['location']
-    if 'language' not in session:
-        session['language'] = request.form['language']
-    if 'comment' not in session:
-        session['comment'] = request.form['comment']
+@app.route('/results', methods=['POST'])
+def results():
+    session['user_name'] = request.form['user_name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    # if request.form['nuggies'] != 'Yes':
+    #     request.form['nuggies'] = 'No'
     
+    session['nuggies'] = request.form.getlist('nuggies')
+    # if request.form['nuggies'] == 'Yes':
+    #     session['nuggies'] = request.form['nuggies']
+    #     print('ttt')
+    # else:
+    #     session['nuggies'] = 'No'
+    session['comment'] = request.form['comment']
     return render_template('results.html')
 
 @app.route('/newform')
