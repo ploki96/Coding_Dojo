14afbdecf957d47ce737aa31756b15276502ad0b
Timothy Wang <timmywang47@yahoo.com>
2021-08-31 11:33:51 -0400

    added base value for health and example ninja

--- MERN/javascript/oop/ninja.js
+++ MERN/javascript/oop/ninja.js
@@ -1,5 +1,5 @@
 class Ninja {
-    constructor(name, health, speed=3, strength=3) {
+    constructor(name, health=50, speed=3, strength=3) {
         this.name = name;
         this.health = health;
         this.speed = speed;
@@ -21,3 +21,7 @@ class Ninja {
         console.log(this.health);
     }
 }
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
