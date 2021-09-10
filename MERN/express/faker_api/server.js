8542cf653412272541574ad28a75aeee057a7edb
Timothy Wang <timmywang47@yahoo.com>
2021-09-10 13:28:50 -0400

    faker api init commit

---
+++ MERN/express/faker_api/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const faker = require('faker');
+const app = express();
+const port = 8000;
+
+class User {
+    constructor(){
+        this._id = faker.datatype.uuid();
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
+    }
+}
+
+class Company {
+    constructor(){
+        this._id = faker.datatype.uuid();
+        this.name = faker.company.companyName();
+        this.address = {
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    }
+}
+
+app.get('/api/users/new', (req, res) => {
+    res.json(new User())
+}); 
+
+app.get("/api/companies/new", (req, res) => {
+    res.json(new Company())
+});
+
+app.get('/api/user/company', (req, res) => {
+    // res.json({user: new User(), company: new Company()})
+    res.json([new User(), new Company()])
+})
+
+const server = app.listen(port, () =>
+    console.log(`Hewwo Fewix`)
+);
+
