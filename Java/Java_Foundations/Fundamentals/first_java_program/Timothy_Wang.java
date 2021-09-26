e948d07ec0ff4106a86827ff56f62598929c42dc
Timothy Wang <timmywang47@yahoo.com>
2021-09-25 20:40:33 -0400

    first java program init commit

---
+++ Java/Java_Foundations/Fundamentals/first_java_program/Timothy_Wang.java
@@ -0,0 +1,7 @@
+public class Timothy_Wang {
+    public static void main(String[] args){
+        System.out.println("My name is Timothy Wang");
+        System.out.println("I am 25 years old.");
+        System.out.println("My hometown is Staten Island, NY");
+    }
+}
