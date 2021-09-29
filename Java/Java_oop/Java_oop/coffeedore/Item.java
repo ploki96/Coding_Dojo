955977637439f493d41afd17afd7d7286efcbb4b
Timothy Wang <timmywang47@yahoo.com>
2021-09-29 15:00:59 -0400

    added this. to a few lines

--- Java/Java_oop/Java_oop/coffeedore/Item.java
+++ Java/Java_oop/Java_oop/coffeedore/Item.java
@@ -1,3 +1,34 @@
 public class Item {
-    
+    private String name;
+    private Double price;
+    private Integer index;
+
+    public Item(String name, Double Price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice () {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getIndex () {
+        return this.index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
 }
--- Java/Java_oop/Java_oop/coffeedore/Order.java
+++ Java/Java_oop/Java_oop/coffeedore/Order.java
@@ -1,3 +1,70 @@
+import java.util.ArrayList;
+
 public class Order {
-    
+
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Items> items = new ArrayList<Items>();
+
+    public Order (){
+        this.name = "guest";
+        this.ready = false;
+    }
+
+    public Order (String name) {
+        this.name = name;
+        this.ready = false;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setReady(Boolean ready){
+        this.ready = ready;
+    }
+
+    public Boolean getReady() {
+        return this.ready;
+    }
+
+    public void setItem(ArrayList<Item> items){
+        this.items = items;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item Item){
+        items.add(Item);
+    }
+
+    public String getStatusMessage() {
+        if (this.ready == true){
+            return "Your order is ready.";
+        } else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal(){
+        double total = 0;
+        for(Item i: this.items){
+            total += i.getPrice();
+        }
+        return total;
+    }
+
+    public void display(){
+        System.out.printf("Customer Name: %s ", this.getName());
+        for (Item i : this.items){
+            System.out.printf("%s - $%.2f ", i.getName(), i.getPrice());
+        }
+    }
 }
