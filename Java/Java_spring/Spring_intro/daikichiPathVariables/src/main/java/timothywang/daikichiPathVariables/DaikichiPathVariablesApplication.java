b0551de40cf4f25860a6f93e748f8481100ea8b4
Timothy Wang <timmywang47@yahoo.com>
2021-10-01 15:24:32 -0400

    daikichi path variables init commit

---
+++ Java/Java_spring/Spring_intro/daikichiPathVariables/src/main/java/timothywang/daikichiPathVariables/DaikichiPathVariablesApplication.java
@@ -0,0 +1,46 @@
+package timothywang.daikichiPathVariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+	}
+	@RequestMapping("")
+
+    public String hello() { 
+            return "Welcome!";
+    }
+    @RequestMapping("/today")
+    public String index(){
+      return "Today you will find luck in all your endeavors!";
+    }
+
+    @RequestMapping("/tomorrow")
+    public String world(){
+      return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+    
+    @RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city) {
+    	return "Congratulations! You will soon travel to " + city;
+    }
+    
+    @RequestMapping("/lotto/{num}")
+    public String lotto(@PathVariable("num") Integer num) {
+    	if (num % 2 == 0) {
+    		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    	} else {
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    	}
+    }
+
+}
---
+++ Java/Java_spring/Spring_intro/daikichiPathVariables/src/test/java/timothywang/daikichiPathVariables/DaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package timothywang.daikichiPathVariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
