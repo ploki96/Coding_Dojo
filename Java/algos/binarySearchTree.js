15c98bb60c1b8b5c07ec777b9b9d0e3371b93e19
Timothy Wang <timmywang47@yahoo.com>
2021-10-15 22:33:28 -0400

    misc algos

--- Java/algos/binarySearchTree.js
+++ Java/algos/binarySearchTree.js
@@ -40,20 +40,20 @@ class BST {
         } 
     }
 
-    findMin() {
-        var runner = this.root;
+    findMin(node = this.root) {
+        var runner = node;
         while (runner.left != null){
             runner = runner.left;
         }
-        return runner.value;
+        return runner;
     }
 
-    findMax() {
-        var runner = this.root;
+    findMax(node = this.root) {
+        var runner = node;
         while (runner.right != null){
             runner = runner.right;
         }
-        return runner.value;
+        return runner;
     }
 
     size(node = this.root) {
@@ -77,7 +77,64 @@ class BST {
         }
         return false;
     }
+
+    maxHeight(node = this.root) {
+        if (node == null) return 0;
+        
+        let leftMax = this.maxHeight(node.left) + 1;
+        let rightMax = this.maxHeight(node.right) + 1;
+
+        if (leftMax > rightMax) {
+            return leftMax;
+        } else {
+            return rightMax;
+        }
+        
+    }
+
+    isBalanced() {
+        let leftSide = this.maxHeight(this.root.left);
+        let rightSide = this.maxHeight(this.root.right);
+
+        if (Math.abs(leftSide - rightSide)> 1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    deleteNode(value, node = this.root) {
+        if (node == null) {
+            return node;
+        }
+        if (node.value > value){
+            node.left = this.deleteNode(value, node.left);
+            console.log(node.left.value)
+        } else if (node.value < value) {
+            node.right = this.deleteNode(value, node.right);
+        } else {
+            if (node.left == null && node.right == null){
+                node == null;
+            } else if (node.left && node.right) {
+                let pred = this.findMax(node.left);
+                node.left = this.deleteNode(pred.value, node.left);
+            } else {
+                let child
+                if (node.left) {
+                    child = node.left
+                    
+                } else {
+                    child = node.right
+                }
+                node = child;
+            }
+        }
+        return node;
+    }
 }
+
+
+    
 
 let newBST = new BST();
 newBST.addNode(7);
@@ -87,8 +144,10 @@ newBST.addNode(4);
 newBST.addNode(1);
 // console.log(newBST.findMax());
 // console.log(newBST.findMin());
-console.log(newBST.contains(1));
-console.log(newBST.contains(8));
-console.log(newBST.contains(2));
-console.log(newBST.size());
+// console.log(newBST.contains(1));
+// console.log(newBST.contains(8));
+// console.log(newBST.contains(2));
+
+console.log(newBST.deleteNode(5));
+console.log(newBST.contains(5))
 
